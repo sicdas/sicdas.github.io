31237f0ae4e90d738c0eaf4f95889fab560946bc
Herminio <herminiopaucar@gmail.com>
2024-11-13 00:14:36 -0500

    upload comming soon page

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,7 +4,7 @@
 
   $(window).on('load', function() {
 
-  /*Page Loader active
+     /* Page Loader active
     ========================================================*/
     $('#preloader').fadeOut();
 
@@ -17,58 +17,24 @@
         }
     });
 
+
+
     /* ==========================================================================
-       countdown timer
-       ========================================================================== */
-     jQuery('#clock').countdown('2023/12/29',function(event){
+    countdown timer
+    ========================================================================== */
+     jQuery('#clock').countdown('2024/12/29',function(event){
       var $this=jQuery(this).html(event.strftime(''
-      +'<div class="time-entry days"><span>%-D</span> <b>:</b> Days</div> '
-      +'<div class="time-entry hours"><span>%H</span> <b>:</b> Hours</div> '
-      +'<div class="time-entry minutes"><span>%M</span> <b>:</b> Minutes</div> '
+      +'<div class="time-entry days"><span>%-D</span> Days</div> '
+      +'<div class="time-entry hours"><span>%H</span> Hours</div> '
+      +'<div class="time-entry minutes"><span>%M</span> Minutes</div> '
       +'<div class="time-entry seconds"><span>%S</span> Seconds</div> '));
     });
 
-    /* Auto Close Responsive Navbar on Click
-    ========================================================*/
-    function close_toggle() {
-        if ($(window).width() <= 768) {
-            $('.navbar-collapse a').on('click', function () {
-                $('.navbar-collapse').collapse('hide');
-            });
-        }
-        else {
-            $('.navbar .navbar-inverse a').off('click');
-        }
-    }
-    close_toggle();
-    $(window).resize(close_toggle);
-
-      /* WOW Scroll Spy
-    ========================================================*/
-     var wow = new WOW({
-      //disabled for mobile
-        mobile: false
-    });
-    wow.init();
-
-    /* Nivo Lightbox 
-    ========================================================*/
-    $('.lightbox').nivoLightbox({
-        effect: 'fadeScale',
-        keyboardNav: true,
-      });
 
     // one page navigation 
-    $('.navbar-nav').onePageNav({
+    $('.onepage-nev').onePageNav({
             currentClass: 'active'
     }); 
-
-    /* Counter
-    ========================================================*/
-    $('.counterUp').counterUp({
-     delay: 10,
-     time: 1500
-    });
 
     /* Back Top Link active
     ========================================================*/
